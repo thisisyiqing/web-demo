198a335c248b1a07c8ace6a7bc8f0b2bc028034b
Yiqing <ywang46@uw.edu>
2022-02-07 17:00:53 -0800

    css style added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import './override_default.css';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Label, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 class App extends React.Component{
 
@@ -32,13 +33,16 @@ class App extends React.Component{
       this.intervalID = setInterval(this.fetchData, 3000);
 
       this.setState({
-        text: 'Data collection started!'
+        text: 'Data collection and transmission started!'
       })
     }
   }
 
   stopInterval = () => {
     clearInterval(this.intervalID);
+    this.setState({
+      text: 'Data collection and transmission stopped!'
+    })
   }
 
   
@@ -46,27 +50,30 @@ class App extends React.Component{
   render() {
     return (
       <div>
-        <Button variant="primary" onClick={this.changeText}>Start</Button>
-        <div>{this.state.text}</div>
-        <Button variant="primary" onClick={this.stopInterval}>Stop</Button>
+        <div style={{display: "flex", justifyContent: "center"}}>
+          <Button variant="primary" onClick={this.changeText} style={{marginTop: "1em", marginRight: "5em", marginBottom: "1em"}}>Start</Button>
+          <Button variant="primary" onClick={this.stopInterval} style={{marginTop: "1em", marginBottom: "1em"}}>Stop</Button>
+        </div>
 
-        <LineChart
-          width = {1200}
-          height = {400}
-          data = {this.state.data}
-          margin = {{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line dataKey="temp" stroke="#8884d8" activeDot={{ r: 8 }} />
-        </LineChart>
+        <div style={{display: "flex", justifyContent: "center", marginBottom: "1em"}}>{this.state.text}</div>
+
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+          <ResponsiveContainer width="80%" height={600}>
+            <LineChart
+              data = {this.state.data}
+              margin = {{ top: 5, right: 30, left: 20, bottom: 5}}
+              >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="time">
+                <Label value="time" offset={-5} position="insideBottom" />
+              </XAxis>
+              <YAxis label={{ value: 'temperature (F)', angle: -90, position: 'insideLeft', offset:25 }} />
+              <Tooltip />
+              <Legend />
+              <Line dataKey="temp" stroke="#8884d8" activeDot={{ r: 5 }} legendType={'none'} dot={false}/>
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     );
   }
